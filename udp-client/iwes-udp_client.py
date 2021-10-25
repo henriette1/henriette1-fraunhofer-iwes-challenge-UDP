3a86da9e89b561afcd399bb37fd119b3b27de763
Markus Graf <1markusgraf@gmail.com>
2021-10-25 12:22:44 +0200

    first commit

---
+++ udp-client/iwes-udp_client.py
@@ -0,0 +1,28 @@
+import socket
+import random
+from time import sleep
+
+serverAddressPort = ("iwes-udp_server", 20001)
+
+bufferSize = 1024
+
+# Create a UDP socket at client side
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+
+while True:
+    sleep(5)
+    message_list = [
+    '$PHOCT,01,192621.400,T,00,080.378,T,-000.106,T,-03.105,T,+00.220,T,+00.084,-00.025,+00.013,+00.235,+00.024,+00.078,-0008.03*09',
+    '$PHOCT,01,192621.500,T,00,080.311,T,-000.237,T,-03.066,T,+00.239,T,+00.105,-00.021,+00.020,+00.172,+00.042,+00.073,-0061.95*0D',
+    '$PHOCT,01,192621.600,T,00,080.274,T,-000.329,T,-02.938,T,+00.252,T,+00.123,-00.017,+00.027,+00.205,+00.048,+00.061,+0001.33*0C',
+    '$PHOCT,01,192621.700,T,00,080.153,T,-000.438,T,-02.926,T,+00.269,T,+00.141,-00.013,+00.032,+00.128,+00.041,+00.047,-0039.36*06']
+
+    randomMessage = message_list[int(random.uniform(0, len(message_list)))]
+    print('to be sent:', randomMessage)
+    bytesToSend = str.encode(randomMessage)
+
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
---
+++ udp-server/iwes-udp_server.py
@@ -0,0 +1,86 @@
+import socket
+from psycopg2 import connect  # published under GNU Lesser General Public License
+import pandas
+import atexit
+
+# init postgres stuff
+table_name = "NMEA"
+connected = False
+
+while not connected:
+    try:
+        conn = connect(
+            dbname="iwes_challenge",
+            user="iwes",
+            host="iwes-postgres",
+            port="5432",
+            password="iwes"
+        )
+        cursor = conn.cursor()
+        print('connection established', conn.status)
+        connected = True
+    except:
+        continue
+
+
+cursor.execute(f"CREATE TABLE IF NOT EXISTS NMEA (id SERIAL PRIMARY KEY, a TEXT, b TEXT, c TEXT, status1 TEXT, d TEXT, e TEXT, status2 TEXT, f TEXT, status3 TEXT, g TEXT, status4 TEXT, HeaveRaw TEXT, status5 TEXT, HeaveLever TEXT, SurgeLever TEXT, SwayLever TEXT, HeaveSpeed TEXT, SurgeSpeed TEXT, SwaySpeed TEXT, HeadingROT TEXT, Checksum TEXT);")
+print('NMEA table created')
+# get column names
+data_frame = pandas.read_sql_query('SELECT * FROM {} LIMIT 0'.format(table_name), conn)
+table_columns = list(pandas.DataFrame.head(data_frame))
+
+# pop sequential primary key id since it is automatically filled
+table_columns.pop(0)
+
+
+# define on quit behavior
+def on_exit():
+    # commit db changes
+    conn.commit()
+
+    # get latest table content
+    cursor.execute(f'SELECT * FROM {table_name}')
+
+    # print table
+    print(table_columns)
+    for i, record in enumerate(cursor):
+        print('\n', record)
+
+    # close the cursor
+    cursor.close()
+
+    # close the connection
+    conn.close()
+
+
+atexit.register(on_exit)
+
+# init UDP Server
+localIP = "iwes-udp_server"
+localPort = 20001
+bufferSize = 1024
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+UDPServerSocket.bind((localIP, localPort))
+print('socket bound')
+
+while True:
+    print('listening')
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+    message = bytesAddressPair[0].decode("utf-8")
+
+    address = bytesAddressPair[1]
+
+    message = '(\'' + message.replace(',','\',\'').replace('*', '\',\'*') + '\')'
+    print('received:', message)
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+
+    try:
+        cursor.execute(f"INSERT INTO {table_name}({','.join(table_columns)}) VALUES {message};")
+        print('message was written to the db successfully')
+    except:
+        print('An exception occurred - maybe the sentence was to short. Try again.')
